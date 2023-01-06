8d4575d7293619e3359ec8e916444a013eb7cacb
Igor-Manko <manko161igor@gmail.com>
2023-01-07 01:07:20 +0300

    Planing

--- HomeWorkOOP_4/src/planing/Main.java
+++ HomeWorkOOP_4/src/planing/Main.java
@@ -1,12 +1,14 @@
 package planing;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Task g = new Task(7, LocalDateTime.now(), "10.02.23", "Nata", "UUU");
         Task b = new Task(1, LocalDateTime.now(), "12.02.23", "Ivan", "High");
         Task a = new Task(2, LocalDateTime.now(), "12.02.23", "Ivan", "Medium");
@@ -16,15 +18,31 @@ public class Main {
         Task f = new Task(6, LocalDateTime.now(), "10.02.23", "Nata", "Low");
 
         List<Task> list = new ArrayList<>();
+        list.add(g);
         list.add(b);
+        list.add(a);
         list.add(c);
         list.add(d);
         list.add(e);
         list.add(f);
-        list.add(a);
-        list.add(g);
+
+        FileWriter cswWriter = new FileWriter("C:\\Users\\admin\\Desktop\\GeekBrains\\Обучение\\" +
+                "1 четверть\\NewJava\\Homework\\HomeWorkOOP_4\\src\\planing\\output.csv");
+
+        for (Task task : list) {
+            cswWriter.append(task.printPlanTask()).append(System.lineSeparator());
+        }
+        cswWriter.close();
 
         list.sort(Comparator.comparing(PlanBook::sortPriority));
+
+        FileWriter csvWriterSort = new FileWriter("C:\\Users\\admin\\Desktop\\GeekBrains\\Обучение\\" +
+                "1 четверть\\NewJava\\Homework\\HomeWorkOOP_4\\src\\planing\\outputSort.csv");
+
+        for (int i = 0; i < list.size(); i++) {
+            csvWriterSort.append(list.get(i).printPlanTask() + System.lineSeparator());
+        }
+        csvWriterSort.close();
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i).printPlanTask());
